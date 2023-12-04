fa6e12f8bb3c81eb260b45a458822115a85bfa22
Jose Pedro <jpyperez@outlook.com>
2023-12-04 21:46:49 +0000

    Adding values to database status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,19 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "sao acima da média" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query(
+    "SELECT count(*) as opened_connections FROM pg_stat_activity;"
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: databaseVersion.rows[0].server_version,
+    max_connections: Number(maxConnections.rows[0].max_connections),
+    opened_connections: Number(openedConnections.rows[0].opened_connections),
+  });
 }
 
 export default status;
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.version).toBeDefined();
+  expect(typeof responseBody.max_connections).toEqual("number");
+  expect(typeof responseBody.opened_connections).toEqual("number");
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.max_connections).toBeGreaterThanOrEqual(
+    responseBody.opened_connections
+  );
 });
