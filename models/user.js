21a575b846e9c7a7ee9fcceb574079cb2366f86e
Jose Pedro <jpyperez@outlook.com>
2025-04-18 10:48:51 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,37 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const foundUser = await runSelectQuery(username);
+
+  return foundUser;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT 
+          *
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -31,13 +63,13 @@ async function create(userInputValues) {
   async function validateUniqueUsername(username) {
     const results = await database.query({
       text: `
-        SELECT 
-          username
-        FROM
-          users
-        WHERE
-          LOWER(username) = LOWER($1)
-        ;`,
+          SELECT 
+            username
+          FROM
+            users
+          WHERE
+            LOWER(username) = LOWER($1)
+          ;`,
       values: [username],
     });
     if (results.rowCount > 0) {
@@ -51,13 +83,13 @@ async function create(userInputValues) {
   async function runInsertQuery(userInputValues) {
     const results = await database.query({
       text: `
-        INSERT INTO 
-          users (username, email, password) 
-        VALUES 
-          ($1, $2, $3)
-        RETURNING
-          *
-        ;`,
+          INSERT INTO 
+            users (username, email, password) 
+          VALUES 
+            ($1, $2, $3)
+          RETURNING
+            *
+          ;`,
       values: [
         userInputValues.username,
         userInputValues.email,
@@ -70,6 +102,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
